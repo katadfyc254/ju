47cc56b6d986bd0d7cd5d72faadd907389753f1f
katadfyc254 <zkate4116@gmail.com>
2025-07-11 23:54:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ catch {
 const mineflayer = require('mineflayer');
 
 // ✅ CONFIG
-const IP = '191.96.231.2';       // Minecraft server IP
-const PORT = 10578;              // Server port
+const IP = '69.30.219.179';       // Minecraft server IP
+const PORT = 1061;              // Server port
 const WEB_PORT = process.env.PORT || 3000; // Fake port for Render/web host
 const PASSWORD = 'Mishra@123';
 
